8c60aa15e65a6fcefd8e58354612ff430a972230
Naish Abbas <naish031@gmail.com>
2025-05-09 16:15:03 +0500

    fix: apostraphe in contact page

--- app/contact/component/contact.tsx
+++ app/contact/component/contact.tsx
@@ -4,16 +4,15 @@ export default function ContactSection() {
   return (
     <div className="mx-auto w-full max-w-2xl">
       <div className="flex flex-col gap-10 md:gap-14">
-        {/* Contact Information Section */}
         <div className="flex flex-col gap-6 md:gap-8">
           <div className="flex flex-col gap-4">
             <p className="text-base leading-normal text-[#09090B] md:text-lg">
               For more details about our services, projects, and the exciting
-              work we're involved in, don't hesitate to reach out. Whether you
+              work we&#39;re involved in, don&#39;t hesitate to reach out. Whether you
               have inquiries about collaborations, partnerships, or any other
-              aspects of our work, we're here to help and would love to connect
+              aspects of our work, we&#39;re here to help and would love to connect
               with you. Get in touch to learn more about the possibilities and
-              let's explore how we can work together.
+              let&#39;s explore how we can work together.
             </p>
             <AnimatedLink href="mailto:someone@example.com?subject=Let%27s%20Connect&body=Hey%2C%20I%20loved%20your%20work%20and%20wanted%20to%20say%20hi.">
               <span className="font-medium text-[#09090B]">
@@ -26,8 +25,8 @@ export default function ContactSection() {
 
           <div className="flex flex-col gap-4">
             <p className="text-base leading-relaxed text-[#09090B] md:text-lg">
-              Want to reach out? If you're exploring job opportunities or
-              collaborations, drop us a line and let's start a conversation.
+              Want to reach out? If you&#39;re exploring job opportunities or
+              collaborations, drop us a line and let&#39;s start a conversation.
             </p>
             <AnimatedLink href="mailto:someone@example.com?subject=Let%27s%20Connect&body=Hey%2C%20I%20loved%20your%20work%20and%20wanted%20to%20say%20hi.">
               <span className="font-medium text-[#09090B]">
